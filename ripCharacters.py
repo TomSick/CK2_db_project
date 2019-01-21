63a9f35cf6224b5a6da390c319a64af0a7066689
TomSick <tomsickenberger@gmail.com>
2019-01-21 18:03:22 -0500

    1/21 regex att

--- ripCharacters.py
+++ ripCharacters.py
@@ -1,12 +1,21 @@
 import json
+import re
 
 # This method is designed to populate a json with the information stored in the character files folder.
 # The method first runs a loop to populate a dictionary called lords with the desired information.
 def name_rip(str):
     myfile = open(str, encoding='Latin-1')
     lords = {}
+    pattern = r"(^\d+)"
+    pattern2 =r"(^\#)(\d+)"
+
     for line in myfile:
-        if line.__contains__(' = {') and not line.__contains__('.'):
+        # if line.__contains__(' = {') and not line.__contains__('.'):
+
+        if re.match(pattern, line) and not line.__contains__('.'):
+            newlord = line.split(' ')[0]
+            lords[newlord] = {}
+        elif re.match(pattern2, line) and not line.__contains__('.'):
             newlord = line.split(' ')[0]
             lords[newlord] = {}
         elif line.__contains__('name='):
@@ -23,9 +32,10 @@ def name_rip(str):
     with open('characters.json') as fp:
         data = json.load(fp)
         data.update(lords)
-    # The characters json is loaded and it's value assigned to a dictionary.
-    # Using the update() method that dictionary is then updated with the contents of lords.
-    # Any keys from the lords dictionary not already in the data dictionary are added while others are updated.
-    # The updated dictionary is then dumped to json.
     with open('characters.json', 'w') as fp:
         json.dump(data,fp,indent=4)
+
+        # The characters json is loaded and it's value assigned to a dictionary.
+        # Using the update() method that dictionary is then updated with the contents of lords.
+        # Any keys from the lords dictionary not already in the data dictionary are added while others are updated.
+        # The updated dictionary is then dumped to json.
